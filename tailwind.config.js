68b1221daa1608c01ecbba7f7293e8152cbb0d21
Leo Gadil <45849962+leogadil@users.noreply.github.com>
2022-05-06 16:23:52 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  engine: 'jest',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
